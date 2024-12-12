de905d5a0c2ea9b61289b527668344dde1947e37
Akhilesh Kumaravel <akhileshkumaravel@Akhileshs-MacBook-Air.local>
2024-12-12 19:09:49 +0400

    last commit

--- golisite/src/constants/index.jsx
+++ golisite/src/constants/index.jsx
@@ -5,12 +5,12 @@ import { ShieldHalf } from "lucide-react";
 import { PlugZap } from "lucide-react";
 import { GlobeLock } from "lucide-react";
 
-import user1 from "../assets/profile-pictures/user1.jpg";
-import user2 from "../assets/profile-pictures/user2.jpg";
-import user3 from "../assets/profile-pictures/user3.jpg";
-import user4 from "../assets/profile-pictures/user4.jpg";
-import user5 from "../assets/profile-pictures/user5.jpg";
-import user6 from "../assets/profile-pictures/user6.jpg";
+import user1 from "../assets/profile-pictures/deb.jpg";
+import user2 from "../assets/profile-pictures/akhil.jpg";
+import user3 from "../assets/profile-pictures/couple.jpg";
+import user4 from "../assets/profile-pictures/Risith.jpg";
+import user5 from "../assets/profile-pictures/suhu.jpg";
+import user6 from "../assets/profile-pictures/veer.jpg";
 
 export const navItems = [
   { label: "Features", href: "#" },
@@ -21,40 +21,40 @@ export const navItems = [
 
 export const testimonials = [
   {
-    user: "John Doe",
-    company: "Stellar Solutions",
+    user: "Ayushmaan Deb",
+    company: "Upcoming cancer patient",
     image: user1,
     text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
   },
   {
-    user: "Jane Smith",
-    company: "Blue Horizon Technologies",
+    user: "Akhil Y.",
+    company: "Upcoming Junkie",
     image: user2,
-    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+    text: "I had like twice, but I feel like it was always part of me, I owe the team my life and my hair",
   },
   {
-    user: "David Johnson",
-    company: "Quantum Innovations",
+    user: "Rachit and Chakrabooty",
+    company: "Well Known drunk power-couple",
     image: user3,
-    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+    text: "Working with this company was a pleasure. Their hiding spots are top notch.Always said I dont want it but I'm usually high the very next hour,I love my s/o",
   },
   {
-    user: "Ronee Brown",
-    company: "Fusion Dynamics",
+    user: "Risith Pai",
+    company: "Akhil BF",
     image: user4,
-    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+    text: "Never Had it but my Boyfriend said its good ,as I trust him with my life I assume its good",
   },
   {
-    user: "Michael Wilson",
-    company: "Visionary Creations",
+    user: "Suhuruth Vippagunta",
+    company: "Upcoming Twink/PocketPussy",
     image: user5,
-    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+    text: "I had this and I was out like a light,I have been just randomly spitting random shit which no sober or high person could even fathom to understand",
   },
   {
-    user: "Emily Davis",
-    company: "Synergy Systems",
+    user: "Veer Aryan",
+    company: "Thambi Media team head",
     image: user6,
-    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+    text: "Lil too good ,I have this more than the times I drink water,Infact when my friends say enough I say the amount of golis I havent had is far greater than the amount I've had",
   },
 ];
 
